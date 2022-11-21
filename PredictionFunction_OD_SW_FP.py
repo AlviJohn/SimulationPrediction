6edcafe22b48b1cf1158e8a9dc13bb64777ba065
Alvi John <aalwiz09@gmail.com>
2022-11-21 15:07:03 +0530

    Update PredictionFunction_OD_SW_FP.py

--- PredictionFunction_OD_SW_FP.py
+++ PredictionFunction_OD_SW_FP.py
@@ -1,15 +1,9 @@
-import collections.abc
-collections.Iterable = collections.abc.Iterable
-collections.Mapping = collections.abc.Mapping
-collections.MutableSet = collections.abc.MutableSet
-collections.MutableMapping = collections.abc.MutableMapping
 from pycaret.regression import *
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
 from PIL import Image
-import evidently
 from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset
 import streamlit.components.v1 as components
@@ -18,6 +12,7 @@ st.set_page_config(page_title="Simulation Prediction Engine", page_icon="musclem
 st.title('SW,OD and Footprint Models')
 st.write('This application predicts SW Inflation, OD Inflation, and Footprint values based on tyre parameters.Please upload the input data template')
 
+#####
 
 
 
@@ -32,6 +27,7 @@ def loadmodel():
     return model_SW,model_OD,Footprint_width, Footprint_length, Footprint_length80
 
 model_SW,model_OD,Footprint_width, Footprint_length, Footprint_length80 =  loadmodel()
+
 Rawdata = pd.read_csv('RawData1.csv')
 ##Correction In Belt_Angle Difference
 Rawdata['Belt_Angle_Difference'] =  abs(Rawdata['RBELT1_cured_angle']) - abs(Rawdata['RBELT2_cured_angle'])
@@ -45,7 +41,11 @@ with st.expander('Update the Data For Prediction'):
 
 	if uploaded_file is not None:
 	    # Can be used wherever a "file-like" object is accepted:
-	    features_df = pd.read_excel(uploaded_file,sheet_name='Template') 
+	    features_df = pd.read_excel(open('Input Data Template_V1.xlsx', 'rb'),sheet_name='Template')
+	    features_df = features_df.dropna(axis=1, how='all')
+	    features_df= features_df.transpose()
+	    features_df.columns = features_df.iloc[0]
+	    features_df= features_df.drop(features_df.index[0]) 
 	    features_df['CuringRim_Width_Difference'] =  abs(features_df['Curing Width_UDMSIteration']) - abs(features_df['Rim Width_UDMSIteration'])
 	    features_df['Belt_Angle_Difference'] =  abs(features_df['RBELT1_cured_angle']) - abs(features_df['RBELT2_cured_angle'])
 	    features_df['Belt_Width_Difference'] =  features_df['Belt Width 1'] - features_df['Belt Width 2']
@@ -53,15 +53,24 @@ with st.expander('Update the Data For Prediction'):
 	    features_df["Aspect_ratio"] =features_df["size"].str.split("/").str[-1].str.extract('(\d+)').astype(int)
 	    st.write('The data you selected is')
 	    st.write(features_df)
-	    st.write('Data also contains dervived variables from data. Please scroll to the right to see')
+	    st.write('Data also contains dervived variables from data. Please scroll to the right to view')
 
 
 
 if uploaded_file is not None:
 	with st.expander('Data Drift Analysis'):
 		if st.button('Plese click for drift calculation'):
+			l1=Rawdata.columns
+			l2=features_df.columns
+			#st.write(list(set(l2) - set(l1)))
+
+			l1=Rawdata.drop('Apex Ending', axis=1)
+			l2=features_df.drop('Apex Ending', axis=1)
+
+			l1 = l1[l2.columns]
+
 			data_drift_report = Report(metrics=[DataDriftPreset(),])
-			data_drift_report.run(current_data=features_df, reference_data=Rawdata, column_mapping=None)
+			data_drift_report.run(current_data=l1, reference_data=l2, column_mapping=None)
 			data_drift_report.save_html("file.html")
 			with open("file.html", "rb") as pdf_file:
 				PDFbyte = pdf_file.read()
@@ -72,24 +81,33 @@ if uploaded_file is not None:
 
 
 
-
-
-if st.button('Please click this button for Prediction'):
-	SW_value = float(round(predict_model(model_SW, features_df),2).Label)
-	OD_value = float(round(predict_model(model_OD, features_df),2).Label)
 	
-	features_df['OD Inflation'] = OD_value + features_df['Cavity Outer Diameter']
-	features_df['SW Inflation'] = SW_value + features_df['Cavity Section Width']
-	features_df['SW Inflation']=200
-	features_df['load'] = 300
-	#FP_width_value = float(round(predict_model(Footprint_width, features_df),2).Label)
-	FP_length_value = float(round(predict_model(Footprint_length, features_df),2).Label)
-	FP_length80_value = float(round(predict_model(Footprint_length80, features_df),2).Label)
-	st.write(' SW Inflation Prediction -->',float(round(features_df['SW Inflation'],2)) )
-	st.write(' OD Inflation Prediction -->',float(round(features_df['OD Inflation'],2)))
-	#st.write(' FP Width Prediction -->',FP_width_value)
-	st.write(' FP Length Prediction -->',FP_length_value)
-	st.write(' FP Length 80 Prediction -->',FP_length80_value)
+	st.caption('Please Select')
+	col1, col2, col3 = st.columns(3)
+
+	with col1:
+		features_df['inflation']= st.slider('Inflation', 180, 420, 210)
+
+	with col3:
+		features_df['load']= st.slider('Load', 100,870,420)    
+
+if st.button('Please Click for Prediction'):
+	
+
+    df= pd.DataFrame()
+    SW_value=round(predict_model(model_SW, features_df),2).Label
+    OD_value = round(predict_model(model_OD, features_df),2).Label
+    df['OD Inflation'] = OD_value + features_df['Cavity Outer Diameter']
+    df['SW Inflation'] = 10 + features_df['Cavity Section Width']
+    features_df['OD Inflation']=df['OD Inflation']
+    features_df['SW Inflation']=df['SW Inflation']
+    
+
+
+    df['FP width'] = round(predict_model(Footprint_width, features_df),2).Label
+    df['FP_length']= round(predict_model(Footprint_length, features_df),2).Label
+    df['FP_length80'] = round(predict_model(Footprint_length80, features_df),2).Label
+    st.write(df)
 
 
 image = Image.open('muscle_man2.png')
