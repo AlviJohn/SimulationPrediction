d6032db03653c15f75e87ebc39d0f83fc42af27c
Alvi John <aalwiz09@gmail.com>
2022-11-22 16:27:29 +0530

    Update PredictionFunction_OD_SW_FP.py

--- PredictionFunction_OD_SW_FP.py
+++ PredictionFunction_OD_SW_FP.py
@@ -107,23 +107,41 @@ if uploaded_file is not None:
 	
 	    try:
 	    	SW_value=round(predict_model(model_SW, features_df),2).Label
+		df['SW Inflation'] = SW_value + features_df['Cavity Section Width']
+		features_df['SW Inflation']=df['SW Inflation']
 	    except:
 	    	st.write("Section Width Model Failed")
+		features_df['SW Inflation'] = features_df['Cavity Section Width']
 
-	    
-	    OD_value = round(predict_model(model_OD, features_df),2).Label
-	    df['OD Inflation'] = features_df['Cavity Outer Diameter'] 
-	    df['SW Inflation'] = SW_value + features_df['Cavity Section Width']
-	    features_df['OD Inflation']=df['OD Inflation']
-	    features_df['SW Inflation']=df['SW Inflation']
+	    try:
+		OD_value = round(predict_model(model_OD, features_df),2).Label
+		df['OD Inflation'] = features_df['Cavity Outer Diameter'] + OD_value
+		features_df['OD Inflation']=df['OD Inflation']
+	    except:
+	    	st.write("OD Model Failed")
+		features_df[''OD Inflation'] = features_df['Cavity Outer Diameter']
+	         
 
+            try:
+		df['FP width'] = round(predict_model(Footprint_width, features_df),2).Label
+	    except:
+	    	st.write("FP Width Model Failed")
 
-
-	    df['FP width'] = round(predict_model(Footprint_width, features_df),2).Label
-	    df['FP_length']= round(predict_model(Footprint_length, features_df),2).Label
-	    df['FP_length80'] = round(predict_model(Footprint_length80, features_df),2).Label
-	    df['FP_Index'] = round(df['FP_length80']/df['FP_length'],2)
-		
+            try:
+		df['FP_length']= round(predict_model(Footprint_length, features_df),2).Label
+	    except:
+	    	st.write("FP Length Model Failed")
+			    
+            try:
+		df['FP_length80'] = round(predict_model(Footprint_length80, features_df),2).Label
+	    except:
+	    	st.write("FP Length 80 Model Failed")
+			    
+            try:
+		df['FP_Index'] = round(df['FP_length80']/df['FP_length'],2
+	    except:
+	    	st.write("FP Index Calculation Failed")
+			    
 		
 	    st.write(df)
 
