71d4a28a10be42f88400fe84f81841443eeccf2c
Alvi John <aalwiz09@gmail.com>
2022-11-24 12:26:18 +0530

    Update PredictionFunction_OD_SW_FP.py

--- PredictionFunction_OD_SW_FP.py
+++ PredictionFunction_OD_SW_FP.py
@@ -117,6 +117,7 @@ if uploaded_file is not None:
 			features_df['OD Inflation']=df['OD Inflation']
 		except:
 			st.write("Error!-OD Model Failed")
+			df['OD Inflation'] = features_df['Cavity Outer Diameter'] + OD_value
 			print("ERORRRRRRRRRRRRRRRRRRRRRRRRRRRR")
 			features_df['OD Inflation'] = features_df['Cavity Outer Diameter']
 		try:
